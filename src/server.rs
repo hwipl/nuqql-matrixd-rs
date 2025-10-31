4c16f2332a5aa6428324b0346b900cafe760df9a
hwipl <33433250+hwipl@users.noreply.github.com>
2025-10-31 17:35:44 +0100

    update tcp server
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,39 @@
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::net::{TcpListener, TcpStream};
 
+async fn send(stream: &mut TcpStream, bytes: &[u8]) -> std::io::Result<()> {
+    stream.write_all(bytes).await
+}
+
+async fn receive(stream: &mut TcpStream) -> std::io::Result<String> {
+    let mut buf = String::new();
+    let mut stream = BufReader::new(stream);
+    loop {
+        let num_bytes = stream.read_line(&mut buf).await?;
+        if num_bytes == 0 {
+            break;
+        }
+        let mut r = false;
+        if let Some((_, '\r')) = buf.char_indices().nth_back(1) {
+            r = true
+        };
+        let mut n = false;
+        if let Some((_, '\n')) = buf.char_indices().nth_back(0) {
+            n = true
+        }
+        if r && n {
+            break;
+        }
+    }
+    return Ok(buf);
+}
+
 async fn handle_client(mut stream: TcpStream) {
+    send(&mut stream, b"Welcome to nuqql-matrixd-rs!\r\n")
+        .await
+        .unwrap();
+    let msg = receive(&mut stream).await.unwrap();
+    println!("{}", msg);
     let (mut reader, mut writer) = stream.split();
     if let Err(e) = tokio::io::copy(&mut reader, &mut writer).await {
         println!("Error reading from client: {}", e);
@@ -8,7 +41,7 @@ async fn handle_client(mut stream: TcpStream) {
 }
 
 pub async fn run_server() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0").await?;
+    let listener = TcpListener::bind("127.0.0.1:32000").await?;
 
     println!("Server listening on: {}", listener.local_addr()?);
 
