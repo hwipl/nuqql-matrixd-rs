8d3176e92419124c7231a0e9a4c8e3fa9fe80aa9
hwipl <33433250+hwipl@users.noreply.github.com>
2025-11-07 14:10:22 +0100

    server: add client channels
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,6 @@
 use tokio::io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader};
 use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::mpsc;
 
 const MAX_MSG_LENGTH: u64 = 128 * 1024;
 
@@ -25,16 +26,33 @@ impl Client {
         }
     }
 
-    async fn handle_client(&mut self) {
+    async fn handle_client(
+        &mut self,
+        from_client: mpsc::Sender<String>,
+        mut to_client: mpsc::Receiver<String>,
+    ) {
         if let Err(err) = Self::send(&mut self.stream, b"Welcome to nuqql-matrixd-rs!\r\n").await {
             println!("Error sending to client: {err}");
         }
         loop {
-            match Self::receive(&mut self.stream).await {
-                Ok(msg) => print!("{msg}"),
-                Err(err) => {
-                    println!("Error receiving from client: {err}");
-                    return;
+            tokio::select! {
+                msg = Self::receive(&mut self.stream) => match msg {
+                    Ok(msg) => {
+                        if let Err(err) = from_client.send(msg).await {
+                            println!("Error sending client message to receive channel: {err}");
+                            return;
+                        }
+                    }
+                    Err(err) => {
+                        println!("Error receiving from client: {err}");
+                        return
+                    }
+                },
+                Some(msg) = to_client.recv() => {
+                    if let Err(err) = Self::send(&mut self.stream, &msg.as_bytes()).await {
+                        println!("Error sending to client: {err}");
+                        return;
+                    }
                 }
             }
         }
@@ -54,6 +72,16 @@ pub async fn run_server() -> std::io::Result<()> {
         let (stream, _) = listener.accept().await?;
 
         // only one client connection is allowed at the same time
-        Client::new(stream).handle_client().await;
+        let (from_client_tx, mut from_client_rx) = mpsc::channel(1);
+        let (to_client_tx, to_client_rx) = mpsc::channel(1);
+        let mut client = Client::new(stream);
+        tokio::spawn(async move { client.handle_client(from_client_tx, to_client_rx).await });
+        while let Some(msg) = from_client_rx.recv().await {
+            print!("{msg}");
+            if let Err(err) = to_client_tx.send(msg).await {
+                println!("Error sending message to send channel: {err}");
+                break;
+            }
+        }
     }
 }
