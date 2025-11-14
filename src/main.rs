372f2b6a65d69d2b506beaa5e9be38884ed5bb19
hwipl <33433250+hwipl@users.noreply.github.com>
2025-11-14 08:34:46 +0100

    server: move run_server() to main
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,64 @@
 mod server;
 
+use server::{Client, Server};
+
+async fn get_message(client: &mut Option<Client>) -> Option<Option<String>> {
+    match client.as_mut() {
+        Some(client) => Some(client.get_message().await),
+        None => None,
+    }
+}
+
+async fn run_server() -> std::io::Result<()> {
+    let server = Server::listen().await?;
+
+    // only one client connection is handled at the same time
+    let mut client = None;
+    loop {
+        tokio::select! {
+            // handle new client connection
+            c = server.next() => match c {
+                Ok(mut c) => {
+                    if client.is_some() {
+                        // client already connected, decline connection
+                        _ = c.send_message("info: client already connected\r\n".into()).await;
+                        continue;
+                    }
+                    if let Err(err) = c.send_message("info: Welcome to nuqql-matrixd-rs!\r\n".into()).await {
+                        println!("Error sending welcome message to client: {err}");
+                        continue;
+                    }
+                    client = Some(c);
+                }
+                Err(err) => {
+                    // server broken?
+                    println!("Error getting client: {err}");
+                    return Err(err);
+                }
+            },
+
+            // handle message from client
+            Some(msg) = get_message(&mut client) => match msg {
+                Some(msg) => {
+                    print!("{msg}");
+                    if let Err(err) = client.as_mut().unwrap().send_message(msg).await {
+                        // client broken?
+                        println!("Error sending message back to client: {err}");
+                        client = None;
+                        continue;
+                    }
+                }
+                None => {
+                    // client broken?
+                    println!("Error getting message from client");
+                    client = None;
+                }
+            }
+        }
+    }
+}
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     println!("Hello from nuqql-matrixd-rs");
-    server::run_server().await
+    run_server().await
 }
--- src/server.rs
+++ src/server.rs
@@ -4,7 +4,7 @@ use tokio::sync::mpsc;
 
 const MAX_MSG_LENGTH: u64 = 128 * 1024;
 
-struct Client {
+pub struct Client {
     from_client: mpsc::Receiver<String>,
     to_client: mpsc::Sender<String>,
 }
@@ -65,84 +65,31 @@ impl Client {
         }
     }
 
-    async fn get_message(&mut self) -> Option<String> {
+    pub async fn get_message(&mut self) -> Option<String> {
         self.from_client.recv().await
     }
 
-    async fn send_message(&mut self, msg: String) -> Result<(), mpsc::error::SendError<String>> {
+    pub async fn send_message(
+        &mut self,
+        msg: String,
+    ) -> Result<(), mpsc::error::SendError<String>> {
         self.to_client.send(msg).await
     }
 }
 
-struct Server {
+pub struct Server {
     listener: TcpListener,
 }
 
 impl Server {
-    async fn listen() -> std::io::Result<Server> {
+    pub async fn listen() -> std::io::Result<Server> {
         let listener = TcpListener::bind("127.0.0.1:32000").await?;
         println!("Server listening on: {}", listener.local_addr()?);
         Ok(Server { listener })
     }
 
-    async fn next(&self) -> std::io::Result<Client> {
+    pub async fn next(&self) -> std::io::Result<Client> {
         let (stream, _) = self.listener.accept().await?;
         Ok(Client::new(stream))
-    }
-}
-
-async fn get_message(client: &mut Option<Client>) -> Option<Option<String>> {
-    match client.as_mut() {
-        Some(client) => Some(client.get_message().await),
-        None => None,
-    }
-}
-
-pub async fn run_server() -> std::io::Result<()> {
-    let server = Server::listen().await?;
-
-    // only one client connection is handled at the same time
-    let mut client = None;
-    loop {
-        tokio::select! {
-            // handle new client connection
-            c = server.next() => match c {
-                Ok(mut c) => {
-                    if client.is_some() {
-                        // client already connected, decline connection
-                        _ = c.send_message("info: client already connected\r\n".into()).await;
-                        continue;
-                    }
-                    if let Err(err) = c.send_message("info: Welcome to nuqql-matrixd-rs!\r\n".into()).await {
-                        println!("Error sending welcome message to client: {err}");
-                        continue;
-                    }
-                    client = Some(c);
-                }
-                Err(err) => {
-                    // server broken?
-                    println!("Error getting client: {err}");
-                    return Err(err);
-                }
-            },
-
-            // handle message from client
-            Some(msg) = get_message(&mut client) => match msg {
-                Some(msg) => {
-                    print!("{msg}");
-                    if let Err(err) = client.as_mut().unwrap().send_message(msg).await {
-                        // client broken?
-                        println!("Error sending message back to client: {err}");
-                        client = None;
-                        continue;
-                    }
-                }
-                None => {
-                    // client broken?
-                    println!("Error getting message from client");
-                    client = None;
-                }
-            }
-        }
     }
 }
