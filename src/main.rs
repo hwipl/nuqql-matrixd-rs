66b707fe0ea71dccc0bbc1f0ddffeea3fc36959d
hwipl <33433250+hwipl@users.noreply.github.com>
2026-02-02 10:49:41 +0100

    add env filter to logging
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,13 @@ mod server;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    tracing_subscriber::fmt::init();
+    let filter = if let Ok(_) = std::env::var("RUST_LOG") {
+        tracing_subscriber::EnvFilter::from_default_env()
+    } else {
+        tracing_subscriber::EnvFilter::new("warn,nuqql_matrixd_rs=info")
+    };
+    tracing_subscriber::fmt::fmt()
+        .with_env_filter(filter)
+        .init();
     daemon::run_daemon().await
 }
