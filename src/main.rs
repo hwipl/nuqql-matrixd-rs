68597f2198c36fb97673c64a4d909b723d7f77d1
hwipl <33433250+hwipl@users.noreply.github.com>
2026-04-05 09:12:13 +0200

    add command line arguments
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,54 @@ mod message;
 mod queue;
 mod server;
 
+use clap::Parser;
+
+const VERSION: &str = "0.1.0";
+
+#[derive(Debug, Parser)]
+#[clap(version = VERSION)]
+struct Args {
+    /// set AF_INET listen address
+    #[clap(long, default_value = "localhost")]
+    address: String,
+
+    /// set socket address family: "inet" for AF_INET, "unix" for AF_UNIX
+    #[clap(long, default_value = "inet")]
+    af: String,
+
+    /// set working directory
+    #[clap(long, default_value = "")]
+    dir: String,
+
+    /// disable message history
+    #[clap(long)]
+    disable_history: bool,
+
+    /// enable filtering of own messages
+    #[clap(long)]
+    filter_own: bool,
+
+    /// set logging level
+    #[clap(long, default_value = "warn")]
+    loglevel: String,
+
+    /// set AF_INET listen port
+    #[clap(long, default_value_t = 32000)]
+    port: u16,
+
+    /// push accounts to client
+    #[clap(long)]
+    push_accounts: bool,
+
+    /// set AF_UNIX socket file in working directory
+    #[clap(long, default_value = "nuqql-matrix.sock")]
+    sockfile: String,
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+    println!("{args:#?}");
     let filter = if std::env::var("RUST_LOG").is_ok() {
         tracing_subscriber::EnvFilter::from_default_env()
     } else {
